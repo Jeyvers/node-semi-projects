99f475f9064bb521474e7e0e08dede0138837bd4
Jeyi Adole <meetjeyvers@gmail.com>
2023-11-26 18:29:21 +0100

    middleware and authorize

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,21 +1,22 @@
 const express = require("express");
 const app = express();
-
+const logger = require("./logger");
+const authorize = require("./authorize");
 //  req => middleware => res
-
-const logger = (req, res, next) => {
- const method = req.method;
- const url = req.url;
- const time = new Date().getFullYear();
- console.log(method, url, time);
- next();
-};
-
-app.get("/", logger, (req, res) => {
+app.use([logger, authorize]);
+// api/home/about/products
+app.get("/", (req, res) => {
  res.send("Home");
 });
-app.get("/about", logger, (req, res) => {
+app.get("/about", (req, res) => {
  res.send("About");
+});
+app.get("/api/products", (req, res) => {
+ res.send("Products");
+});
+app.get("/api/items", (req, res) => {
+ console.log(req.user);
+ res.send("Items");
 });
 
 app.listen(5000, () => {
